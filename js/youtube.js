dfe41648a2980630cf9eb45a33eb3d196b187452
김권일 <rnjsdlf0322@gmail.com>
2022-02-08 13:26:40 +0900

    init

--- js/youtube.js
+++ js/youtube.js
@@ -1,4 +1,5 @@
 // 2. This code loads the IFrame Player API code asynchronously.
+// Youtube IFrame API를 비동기로 로드합니다.
 var tag = document.createElement('script');
 
 tag.src = "https://www.youtube.com/iframe_api";
@@ -7,6 +8,10 @@ firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
 // 3. This function creates an <iframe> (and YouTube player)
 //    after the API code downloads.
+// onYouTubePlayerAPIReady 함수 이름은,
+// Youtube IFrame Player API에서 사용하는 이름이기 때문에,
+// 다르게 지정하면 동작하지 않습니다!
+// 그리고 함수는 전역(Global) 등록해야 합니다!
 function onYouTubeIframeAPIReady() {
     //<div id="player"><div> 
     new YT.Player('player', {
@@ -14,7 +19,12 @@ function onYouTubeIframeAPIReady() {
     playerVars: { // 영상을 제생하기위한 변수
         autoplay : true, // 자동 재생 유무
         loop: true, // 반복 재생 유무 
-        playlist: 'An6LvWQuj_8' // 반복 재생할 유튜브 영상 ID 목록
+        playlist: 'An6LvWQuj_8' // 반복 재생할 유튜브 영상 ID 목록을 다시 선언
+    },
+    events: {
+      onReady: function (event) {
+        event.target.mute(); // 음소거
+      }
     }
   });
 }
