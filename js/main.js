a9baf3bece5053cbdab413c2676e84c0d838afa0
김권일 <rnjsdlf0322@gmail.com>
2022-02-10 13:45:01 +0900

    init

--- js/main.js
+++ js/main.js
@@ -16,38 +16,45 @@ searchInputEl.addEventListener('blur', function(){
     searchInputEl.setAttribute('placeholder', '');
 });
 
-const badgeEl = document.querySelector('header .badges');
-
-window.addEventListener('scroll', _.throttle(function () {
-    console.log(window.scrollY);
-    if (window.scrollY > 500) {
-        // 배지 숨기기
-        // gsap.to(요소, 지속시간, 옵션)
-        gsap.to(badgeEl, .6, {
-            opacity: 0,
-            display: 'none'
-        });
-    } else {
-        // 배지 보이기
-        gsap.to(badgeEl, .6, {
-            opacity: 1,
-            display: 'block'
-        });
-    }
-}, 300));
-// _.throttle(함수, 시간)
-
 /**
- * 순서대로 나타나는 기능
+ * 페이지 스크롤에 따른 요소 제어
  */
-// 나타날 요소들(.fade-in) 찾기.
-const fadeEls = document.querySelectorAll('.visual .fade-in')
-// 나타날 요소들을 하나씩 반복해서 처리!
-fadeEls.forEach(function (fadeEl, index) {
-  // 각 요소들을 순서대로(delay) 보여지게 함!
-  gsap.to(fadeEl, 1, {
-    delay: (index + 1) * .7,
-    opacity: 1
+// 페이지 스크롤에 영향을 받는 요소들을 검색!
+const badgeEl = document.querySelector('header .badges')
+const toTopEl = document.querySelector('#to-top')
+// 페이지에 스크롤 이벤트를 추가!
+// 스크롤이 지나치게 자주 발생하는 것을 조절(throttle, 일부러 부하를 줌)
+window.addEventListener('scroll', _.throttle(function () {
+  // 페이지 스크롤 위치가 500px이 넘으면.
+  if (window.scrollY > 500) {
+    // Badge 요소 숨기기!
+    gsap.to(badgeEl, .6, {
+      opacity: 0,
+      display: 'none'
+    })
+    // 상단으로 스크롤 버튼 보이기!
+    gsap.to(toTopEl, .2, {
+      x: 0
+    })
+
+  // 페이지 스크롤 위치가 500px이 넘지 않으면.
+  } else {
+    // Badge 요소 보이기!
+    gsap.to(badgeEl, .6, {
+      opacity: 1,
+      display: 'block'
+    })
+    // 상단으로 스크롤 버튼 숨기기!
+    gsap.to(toTopEl, .2, {
+      x: 100
+    })
+  }
+}, 300))
+// 상단으로 스크롤 버튼을 클릭하면,
+toTopEl.addEventListener('click', function () {
+  // 페이지 위치를 최상단으로 부드럽게(0.7초 동안) 이동.
+  gsap.to(window, .7, {
+    scrollTo: 0
   })
 })
 
@@ -76,6 +83,18 @@ new Swiper('.promotion .swiper-container', {
         nextEl: '.promotion .swiper-next' // 다음 버튼 선택자
       }
 });
+
+new Swiper('.awards  .swiper-container', {
+    autoplay: true,
+    loop: true,
+    slidesPerView: 5, 
+    spaceBetween: 30, 
+    navigation: {
+        prevEl: '.awards .swiper-prev', // 이전 버튼 선택자
+        nextEl: '.awards .swiper-next' // 다음 버튼 선택자
+    }
+})
+
 // promotion 숨김 처리와 보임 처리
 const promotionEl = document.querySelector('.promotion');
 const promotionToggleBtn = document.querySelector('.toggle-promotion');
@@ -114,3 +133,20 @@ function floatingObject(selector, delay, size){
 floatingObject('.floating1', 1, 15);
 floatingObject('.floating2', .5, 15);
 floatingObject('.floating3', 1.5, 20);
+
+const spyEls = document.querySelectorAll('section.scroll-spy');
+spyEls.forEach(function (spyEl) {
+    new ScrollMagic
+        .Scene({
+            triggerElement: spyEl, // 보여짐 여부를 감시 할 요서를 지정
+            triggerHook: .8
+        })
+        .setClassToggle(spyEl, 'show')
+        .addTo(new ScrollMagic.Controller())
+});
+
+/**
+ * 올해가 몇 년도인지 계산
+ */
+ const thisYear = document.querySelector('.this-year')
+ thisYear.textContent = new Date().getFullYear()
